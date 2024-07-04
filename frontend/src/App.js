f3c84dda4e7928c7c201415c7f8a5c64ff2e7e12
Matheus <matheuzhenrik@gmail.com>
2024-07-04 16:27:11 -0300

    feat(frontend): register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./Login";
+import Register from "./Register";
 import Measurements from "./Measurements";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/measurements" element={<Measurements />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Login.css"; // Import your CSS file
 
-const Login = () => {
+const Register = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -10,14 +11,15 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("https://matheusmedeor.pythonanywhere.com/login", {
-        email,
+      .post("https://matheusmedeor.pythonanywhere.com/users", {
+        name,
         password,
+        email,
       })
       .then((response) => {
         console.log(response.data);
         if (response.data.user) {
-          window.location.href = "/measurements"; // Redirect upon successful login
+          window.location.href = "/";
         }
       })
       .catch((error) => {
@@ -30,6 +32,15 @@ const Login = () => {
       <div className="login-container">
         <h2 className="login-title">Biodigestor</h2>
         <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Nome:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
           <div className="form-group">
             <label>Email:</label>
             <input
@@ -49,21 +60,13 @@ const Login = () => {
             />
           </div>
           <button type="submit" className="submit-btn">
-            Entrar
+            Cadastrar
           </button>
-
           {error && <p className="error-message">{error}</p>}
         </form>
-        <button
-          type="submit"
-          className="submit-btn"
-          onClick={() => (window.location.href = "/register")}
-        >
-          Registrar
-        </button>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
